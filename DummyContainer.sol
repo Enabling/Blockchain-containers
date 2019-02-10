9efcad4c15900c5b39ea3015132278a23ede66fd
Maarten Dhondt <maarten.dhondt@skynet.be>
2019-02-10 13:11:49 +0100

    Fix dummy contract

--- DummyContainer.sol
+++ DummyContainer.sol
@@ -2,35 +2,35 @@ pragma solidity ^0.4.24;
 
 contract DummyContainer {
 
-    uint256 id;
-    bool    doorsOpen;
-    address owner;
+    uint256 _id;
+    bool    _doorsOpen;
+    address _owner;
 
     event DoorsOpened();
     event DoorsClosed();
 	
-	constructor DummyContainer() {
-		id = 666;
-		doorsOpen = false;
-		owner = tx.origin;
+	constructor() {
+		_id = 666;
+		_doorsOpen = false;
+		_owner = tx.origin;
 	}
 
     modifier onlyOwner() {
-        require(tx.origin == owner); // Do not use tx.origin in production code: https://github.com/ethereum/solidity/issues/683
+        require(tx.origin == _owner); // Do not use tx.origin in production code: https://github.com/ethereum/solidity/issues/683
         _;
     }
 
     function getId() public view returns (uint256 id) {
-        return id;
+        return _id;
     }
 
     function areDoorsOpen() public view returns (bool doorsOpen) {
-        return doorsOpen;
+        return _doorsOpen;
     }
 
-    function setDoorsOpen(bool doorsOpened) public onlyOwner {
-        doorsOpen = doorsOpened;
-        if (doorsOpen) {
+    function setDoorsOpen(bool doorsOpen) public onlyOwner {
+        _doorsOpen = doorsOpen;
+        if (_doorsOpen) {
             emit DoorsOpened();
         } else {
             emit DoorsClosed();
